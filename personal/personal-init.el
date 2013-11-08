6d4c120c3dcf4b7614faf8ea8f3d3149a2a45fd3
Mike Simpson <mike.simpson@scimedsolutions.com>
2013-11-07 20:00:39 -0500

    Add personal configurations.

---
+++ personal/personal-init.el
@@ -0,0 +1,15 @@
+;;; Initialize --- Provides local customizations
+
+;;; Commentary:
+
+;;; Code:
+;(add-to-list 'load-path "~/elisp/config/")
+
+(require 'org)
+(org-babel-load-file "~/.emacs.d/readme.org")
+
+;(require 'rss-config)
+;(require 'social-config)
+
+(provide 'personal-init)
+;;; personal-init ends here
