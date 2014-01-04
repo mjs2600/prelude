94b02d37fa47147594c33a8bdff36aa324b81f02
Mike Simpson <mike.simpson@scimedsolutions.com>
2014-01-04 11:13:12 -0500

    Remove compiled literate ELisp file after evaluation

--- personal/personal-init.el
+++ personal/personal-init.el
@@ -6,7 +6,12 @@
 ;(add-to-list 'load-path "~/elisp/config/")
 
 (require 'org)
+
 (org-babel-load-file "~/.emacs.d/personal/readme.org")
+
+(let ((compiled-elisp "~/.emacs.d/readme.el"))
+  (if (file-exists-p compiled-elisp)
+      (delete-file compiled-elisp)))
 
 ;(require 'rss-config)
 ;(require 'social-config)
